9259ec31c71fd48a03f059eed2312378e221af2d
Eiman Hamdy <eimanhamdy99@gmail.com>
2024-02-17 21:23:19 +0200

    Create logic for showing & hiding add-challenge form

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// To be loaded directly.
+const addChallenge = document.querySelector(".add-btn");
+const challengeForm = document.getElementById("challenge-form");
+const closeBtn = document.getElementById('close-btn');
+
+const onClick = () => {
+    challengeForm.style.display="block";
+}
+
+const closeForm = () => {
+    challengeForm.style.display="none";
+}
+
+addChallenge.addEventListener('click',onClick);
+closeBtn.addEventListener('click',closeForm);
